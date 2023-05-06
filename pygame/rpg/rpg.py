a0fa9e8ce9a6c5a9c6c00bf2ee6f6e279ce9b57f
Cameron Astor <cameron.astor@gmail.com>
2023-05-05 23:57:38 -0700

    rpg camera, etc.

--- pygame/rpg/rpg.py
+++ pygame/rpg/rpg.py
@@ -71,23 +71,10 @@ class Tilemap(Entity):
         self.map = [[]]
         self.tilesize = self.tileset.scaled_size # Size of each tile in the map
 
-        TESTMAP =      [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                        [2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
         # Create map tiles from spec
         x_coord = 0
         y_coord = 0
-        for y in TESTMAP:
+        for y in map:
             row = []
             for x in y:
 
@@ -103,10 +90,42 @@ class Tilemap(Entity):
     def update(self, dt) -> None:
         pass
 
-    def render(self, screen: pygame.Surface) -> None:
+    def render(self, screen: pygame.Surface, camera_adj: tuple) -> None:
         for y in self.map:
             for x in y:
-                screen.blit(x.sprite, (x.x, x.y))
+                screen.blit(x.sprite, (x.x + camera_adj[0], x.y + camera_adj[1]))
+
+###################
+## Camera system ##
+###################
+
+# Represents the camera in the RPG. It centers on the subject.
+# Subject must have an x and y property.
+class Camera:
+    def __init__(self, screen: pygame.Surface, subject) -> None:
+        self.screen = screen
+        self.screen_w = screen.get_width()
+        self.screen_h = screen.get_height()
+
+        self.subject = subject
+
+        self.camera_adjustment_x = 0
+        self.camera_adjustment_y = 0
+
+        # Initial camera adjustment
+        # Place subject in center of camera
+        self.camera_adjustment_x = (self.screen_w/2) - self.subject.x
+        self.camera_adjustment_y = (self.screen_h/2) - self.subject.y
+
+    def get_camera_adjustment(self) -> tuple:
+        return (self.camera_adjustment_x, self.camera_adjustment_y)
+
+    def update(self, dt) -> None:
+        # Update camera adjustment based on new subject position
+        self.camera_adjustment_x = (self.screen_w/2) - self.subject.x
+        self.camera_adjustment_y = (self.screen_h/2) - self.subject.y
+
+    
 
 ######################
 ## Animation system ##
@@ -124,27 +143,62 @@ class AnimationManager:
         # and a list of sprite ids as a value.
         self.animations = {}
 
+        self.current_sprite_id = 0 # The current sprite to be displayed given as an id in the tileset
+
+        self.active_animation = None
+        self.active_animation_frequency = 0
+        self.loop_animation = False
+        self.current_animation_keyframe = 0 
+        self.keyframe_time = 0 # For delta time
+
     # Register animation.
     def register_animation(self, name: str, sprite_ids: list[int]):
-        self.animations["name"] = sprite_ids
+        self.animations[name] = sprite_ids
+
+    # Register a single sprite
+    def register_sprite(self, name: str, sprite_id: int):
+        self.animations[name] = sprite_id
+
+    def set_sprite(self, name: str):
+        self.current_sprite_id = self.animations[name]
 
     # Get current animation frame
     def get_current_sprite(self) -> pygame.Surface:
-        return self.tileset.get_tile_sprite(0)
+        return self.tileset.get_tile_sprite(self.current_sprite_id)
 
     # Update animation frame
     def update(self, dt):
-        pass
+
+        if self.active_animation is not None: # If an animation is currently playing
+            self.keyframe_time += dt # Keeping track of how long the current animation frame has been displayed
+
+            if self.keyframe_time >= self.active_animation_frequency: 
+                if len(self.animations[self.active_animation]) - 1 <= self.current_animation_keyframe: # End of this run of animation
+
+                    if self.loop_animation is True:
+                        self.current_animation_keyframe = 0
+                    else:
+                        self.deactivate_animation()
+                else: 
+                    self.current_animation_keyframe += 1 # Move to next keyframe
+                    self.current_sprite_id = self.animations[self.active_animation][self.current_animation_keyframe] # Assign sprite at new keyframe
+                    
+                self.keyframe_time = 0 # Reset keyframe time before ending animation
+
 
     # Plays a single desired animation for this entity.
-    def play_single_animation(self, animation: str, frequency: int):
-        pass
+    # Overrides current animation
+    def activate_animation(self, animation: str, frequency: float, loop: bool):
+        self.active_animation = animation
+        self.active_animation_frequency = frequency
+        self.loop_animation = loop
 
-    def activate_loop_animation(self, animation: str, frequency: int):
-        pass
+    def deactivate_animation(self):
+        self.active_animation = None
+        self.active_animation_frequency = 0
+        self.loop_animation = False
 
-    def deactivate_loop_animation(self, animation: str):
-        pass
+
 
 ##############
 ## Entities ##
@@ -154,15 +208,59 @@ class Player(Entity):
     def __init__(self, spritesheet: pygame.Surface, x, y) -> None:
         self.x = x
         self.y = y
+        self.velocity = 250
+        self.direction = "down" # Direction the player is currently pointing
+        self.moving = False
 
-        # Register animations
+        #########################
+        ## Register animations ##
+        #########################
         self.animations = AnimationManager(spritesheet)
 
+        # Walking animations
+        self.animations.register_animation("walking_right", [3, 7, 11, 15])
+        self.animations.register_animation("walking_left", [2, 6, 10, 14])
+        self.animations.register_animation("walking_up", [1, 5, 9, 13])
+        self.animations.register_animation("walking_down", [0, 4, 8, 12])
+
+        # Stationary sprites
+        self.animations.register_sprite("stationary_down", 0)
+        self.animations.register_sprite("stationary_up", 1)
+        self.animations.register_sprite("stationary_left", 2)
+        self.animations.register_sprite("stationary_right", 3)
+
+        self.rect = self.animations.get_current_sprite().get_rect()
+
+    def move(self, dt) -> None:
+        if self.direction == "up":
+            self.y -= self.velocity * dt
+        elif self.direction == "down":
+            self.y += self.velocity * dt       
+        elif self.direction == "left":
+            self.x -= self.velocity * dt      
+        elif self.direction == "right":
+            self.x += self.velocity * dt
+
+    def set_direction(self, new_direction: str) -> None:
+        self.direction = new_direction
+
+    def stop_moving(self) -> None:
+        self.moving = False
+        self.animations.set_sprite("stationary_" + self.direction)
+
+    def update_collider(self) -> None:
+        self.rect.x = self.x
+        self.rect.y = self.y
+
     def update(self, dt) -> None:
-        pass
+        if self.moving:
+            self.move(dt)
+
+        self.animations.update(dt)
+        self.update_collider()
     
-    def render(self, screen: pygame.Surface) -> None:
-        screen.blit(self.animations.get_current_sprite(), (self.x, self.y))
+    def render(self, screen: pygame.Surface, camera_adj: tuple) -> None:
+        screen.blit(self.animations.get_current_sprite(), (self.x + camera_adj[0], self.y + camera_adj[1]))
 
 class Enemy(Entity):
     pass
@@ -177,14 +275,39 @@ class StartScene(Scene):
         self.entities = []
         self.previous_time = None
 
+        # MAP
+        MAP     =      [[101,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91,91], 
+                        [81,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                        [81,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                        [81,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,71, 0, 0, 0, 0, 0], 
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0,71, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,71, 0, 0, 0, 0, 0],
+                        [81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [112,69,69,69,69,69,69,69,69,69,69,69,69,69,69,69,69,69,69,69]]
+
         # Create tileset
-        self.tileset = Tileset("gfx/test_tiles.png", 16, 4)
+        self.tileset = Tileset("gfx/rpg_sprites.png", 16, 4)
         # Create tilemap
-        self.tilemap = Tilemap([[]], self.tileset)
+        self.tilemap = Tilemap(MAP, self.tileset)
         # Create player
         self.player = Player(self.sprites["player_animations"], 50, 50)
+        # Create camera
+        self.camera = Camera(self.screen, self.player)
 
-        self.entities.extend([self.tilemap, self.player])
+        #######################
+        ## USER INPUT SYSTEM ##
+        #######################
+        self.keybinds = {pygame.K_w: (0, "up"),
+                         pygame.K_d: (270, "right"),
+                         pygame.K_s: (180, "down"),
+                         pygame.K_a: (90, "left")}
+        
+        self.keystack = []
+        self.current_key = None
 
     def update(self) -> None:
 
@@ -196,15 +319,18 @@ class StartScene(Scene):
         dt = now - self.previous_time
         self.previous_time = now 
 
-        for e in self.entities:
-            e.update(dt)
+        self.tilemap.update(dt)
+        self.player.update(dt)
+        self.camera.update(dt)
     
     def render(self) -> None:
         # Clear screen
-        self.screen.fill("black")
+        self.screen.fill((30, 124, 184)) # Water background color
 
-        for e in self.entities:
-            e.render(self.screen)
+        # for e in self.entities:
+        #     e.render(self.screen)
+        self.tilemap.render(self.screen, self.camera.get_camera_adjustment())
+        self.player.render(self.screen, self.camera.get_camera_adjustment())
 
         # Update display
         pygame.display.update()
@@ -214,6 +340,42 @@ class StartScene(Scene):
 
             if event.type == pygame.QUIT: # If the user closes the window
                 self.manager.quit_game()
+
+            if event.type == pygame.KEYDOWN and event.key in self.keybinds:
+
+                # The moment a key is pressed, add it to the keystack
+                self.keystack.append(event.key)
+                print(self.keystack)
+
+
+            
+            if event.type == pygame.KEYUP and event.key in self.keybinds:
+
+                # If the key is released, remove it from the keystack
+                self.keystack.remove(event.key)
+                print(self.keystack)
+
+                # if self.keybinds[event.key][1] == self.player.direction:
+                #     self.player.moving = False
+
+            # Read latest element in keystack
+            if len(self.keystack) > 0: # If keystack has any keys
+                if self.current_key != self.keystack[-1]: # If the active key has changed
+                    # Deactivate animation
+                    self.player.animations.deactivate_animation()
+
+                    self.current_key = self.keystack[-1]
+
+                    # Player commands
+                    self.player.set_direction(self.keybinds[self.current_key][1])
+                    self.player.animations.activate_animation("walking_" + self.keybinds[event.key][1], 0.2, True)
+                    self.player.moving = True
+
+            if len(self.keystack) == 0:
+                self.current_key = None
+                self.player.stop_moving()
+
+
 
 class Level1(Scene):
     def __init__(self, manager: SceneManager, screen: pygame.Surface, sprites: dict, debug: bool) -> None:
@@ -301,6 +463,7 @@ class Game:
         sprites = {}
 
         sprites["player_animations"] = pygame.image.load("gfx/player_animations.png").convert_alpha()
+        sprites["environment_tiles"] = pygame.image.load("gfx/rpg_sprites.png").convert_alpha()
 
         return sprites
     
